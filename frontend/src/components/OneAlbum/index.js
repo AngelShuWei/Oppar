a0d065a100f18a1f9cff331387367bc654605bc4
angel <angel.wei101@gmail.com>
2022-03-10 21:38:20 -0800

    added css to albums page

--- frontend/src/components/OneAlbum/index.js
+++ frontend/src/components/OneAlbum/index.js
@@ -7,29 +7,33 @@ import { Redirect, NavLink, Link} from "react-router-dom";
 function OneAlbum({photos}) {
   const { albumId } = useParams();
   const sessionUser = useSelector((state) => state.session.user);
-  const albums = useSelector((state) => state.albums[albumId])
+  const albums = useSelector((state) => state.albums[albumId]) //state.albums is an object w/ all albums, key into the specific album object (singular object) {id, userId, title..}
+  // console.log(albums)
 
   if (!sessionUser) return (
     <Redirect to='/'/>
   )
 
-  const userAlbum = photos.filter(photo => photo.albumId === albums.id)
+  const userAlbum = photos.filter(photo => photo.albumId === albums.id) //returns an ARRAY of photo objects. album.id gives the integer id for the specific album obj
+  console.log(userAlbum);
 
   return (
-    <>
-      <img className='flower-background' src={flowerBackground} alt='flower'/>
-        <h1>{albums.title}</h1>
-        <h3>{albums.content}</h3>
-        <div className='user-album-page-container'>
-          <div className='user-albums-container'>
+    <div className='view-album-page-container'>
+      <img className='album-background' src={userAlbum[0].imageUrl} alt='flower'/>
+        <div className='view-album-text'>
+          <h1 className='view-album-title-text'>{albums.title}</h1>
+          <h3 className='view-album-desc-text'>{albums.content}</h3>
+        </div>
+        {/* <div className='user-album-page-container'> */}
+          <div className='view-album-container'>
             {userAlbum.map(photos => (
               <div key={photos.id}>
-                <img className='user-album' src={photos.imageUrl} alt={photos.title}></img>
+                <img className='view-album' src={photos.imageUrl} alt={photos.title}></img>
               </div>
             ))}
           </div>
-        </div>
-    </>
+        {/* </div> */}
+    </div>
   )
 }
 
