757cf16a4b8712e7a578496a4f290947e81ae483
angel <angel.wei101@gmail.com>
2022-03-11 02:37:19 -0800

    testing

---
+++ frontend/src/components/EditAlbumFormPage/index.js
@@ -0,0 +1,7 @@
+function EditAlbumFormPage() {
+  return (
+    <h1></h1>
+  )
+}
+
+export default EditAlbumFormPage;
---
+++ frontend/src/components/UserAlbums/EditDeleteAlbumButton.js
@@ -0,0 +1,22 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { deleteAlbum } from '../../store/albums';
+import { Link } from 'react-router-dom';
+
+function EditDeleteAlbumButton({ album }) {
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <Link to={`/albums/${album.id}/edit`}>
+        <button className='edit-delete-button'>
+          <i className="fa-lg fa-solid fa-pen-to-square" />
+        </button>
+      </Link>
+      {/* <button className='edit-delete-button' onClick={() => dispatch(deleteAlbum(album.id))}>
+        <i className="fa-lg fa-solid fa-trash-can"></i>
+      </button> */}
+    </div>
+  )
+}
+
+export default EditDeleteAlbumButton;
