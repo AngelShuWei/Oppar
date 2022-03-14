5d2db055d6a40b25a7de820c6908cdaa9a077998
angel <angel.wei101@gmail.com>
2022-03-13 18:49:06 -0700

    finished icon footer

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -27,12 +27,14 @@ function Footer () {
         <SiHeroku className='tech-icon' size={30} style={{ color: `rgb(103, 98, 166)`}}/>
         <SiCss3 className='tech-icon' size={30} style={{ color: `rgb(21, 114, 182)` }}/>
         <SiHtml5 className='tech-icon' size={30} style={{ color: `rgb(228, 77, 38)` }}/>
-        <a href='https://github.com/AngelShuWei'>
-          <UilGithub className='tech-icon' id='github' size={30} style={{ color: `rgb(255, 255, 255)` }}/>
-        </a>
-        <a href='https://www.linkedin.com/in/angel-wei-21952b16a/'>
-          <UilLinkedin className='tech-icon' size={30} style={{ color: `rgb(255, 255, 255)` }}/>
-        </a>
+        <div className='social-links'>
+          <a href='https://github.com/AngelShuWei'>
+            <UilGithub className='social-icon' id='github' size={30} style={{ color: `rgb(255, 255, 255)` }}/>
+          </a>
+          <a href='https://www.linkedin.com/in/angel-wei-21952b16a/'>
+            <UilLinkedin className='social-icon' id='linkedin' size={30} style={{ color: `rgb(255, 255, 255)` }}/>
+          </a>
+        </div>
       </div>
       {/* <div className='social-links'>
         <UilGithub size={30}/>
