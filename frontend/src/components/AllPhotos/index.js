c2c3a0c121adbbee9a78e0bfd8fc9610c75916db
angel <angel.wei101@gmail.com>
2022-06-02 01:46:26 -0700

    added css to main page

--- frontend/src/components/AllPhotos/index.js
+++ frontend/src/components/AllPhotos/index.js
@@ -1,4 +1,5 @@
 import './AllPhotos.css'
+import tack from '../../assets/tack.png'
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { NavLink, Route, Link } from 'react-router-dom';
@@ -20,11 +21,15 @@ function AllPhotos() {
         {allPhotos.map(photo =>
           <div className='photo-container' key={photo.id}>
             <Link to={`/photos/${photo.id}`}>
+              {/* <img className='tack' src={tack}/> */}
               <div className='photo-card-top'>
                 <img className='photo' src={photo.imageUrl} alt={photo.title}/>
               </div>
               <div className='photo-card-bottom'>
-                <i className='fa-lg fa-solid fa-heart' id='photo'>{photo.Likes.length}</i>
+                <div className='photo-card-love'>
+                  <span className='photo-love-count' id='photo'>{photo.Likes.length}</span>
+                  <span className='fa-lg fa-solid fa-heart' id='photo'/>
+                </div>
                 <div className='photo-text'>{photo.content}</div>
               </div>
             </Link>
