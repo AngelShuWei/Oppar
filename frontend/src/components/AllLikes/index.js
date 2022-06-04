ed064512b428af7ae36901a8b14533c99fe5abe4
angel <angel.wei101@gmail.com>
2022-06-03 23:45:31 -0700

    finished css on loves page

--- frontend/src/components/AllLikes/index.js
+++ frontend/src/components/AllLikes/index.js
@@ -3,7 +3,7 @@ import { useParams } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux';
 import { Redirect, NavLink, Link} from "react-router-dom";
 import { useEffect, useState} from 'react';
-import { getAllLikes } from '../../store/likes';
+import { deleteLike, getAllLikes } from '../../store/likes';
 
 function AllLikes() {
   const dispatch = useDispatch();
@@ -26,7 +26,7 @@ function AllLikes() {
       <div className='all-img-likes-container'>
         {oneLike.map(like =>
           <div className='one-img-like-container' key={like.id}>
-              <span className='fa-lg fa-solid fa-heart' id='love'/>
+            <div className='fa-lg fa-solid fa-heart' id='love' onClick={() => dispatch(deleteLike(like.id))}/>
             <Link to={`/photos/${like?.Photo?.id}`}>
               <img className='like-photo' src={like?.Photo?.imageUrl}/>
             </Link>
